335e539a9db39c8bb9c1375d497683b96636d5e9
Avinash Shukla <avinashukla2000@gmail.com>
2022-10-26 19:42:29 +0530

    Filled Data

---
+++ src/content_option.js
@@ -0,0 +1,182 @@
+const logotext = "Avinash";
+const meta = {
+  title: "Avinash Shukla",
+  description:
+    "I’m Avinash Shukla Developer _ Full stack devloper,currently working in India",
+};
+
+const introdata = {
+  title: "I’m Avinash Shukla",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop mobile apps",
+  },
+  description: `
+    I am progressive minded and in tune with new developments in my field.
+    I enjoy collective brainstorming sessions which allow me to coordinate activities to achieve a common goal.
+  `,
+  your_img_url: "./assets/images/imaage.jpg",
+};
+
+const dataabout = {
+  title: "A bit about my self",
+  aboutme: `I Love to work with "MERN" Stack. I have solved over 800 questions on leetcode and have also participated in many coding contests on codechef, codeforces. 
+    I am Rank 1 in Geek for geeks College Ranking. I also have Intership Experience on building React Apps`,
+};
+const worktimeline = [
+  {
+    jobtitle: "ReactJs Intern",
+    where: "MobileFirst Applications",
+    date: "Sep - Present",
+  },
+  {
+    jobtitle: "Full Stack Web Developer",
+    where: "River House Technology",
+    date: "July - Oct 2021",
+  },
+];
+
+const skills = [
+  {
+    name: "C++",
+    value: 80,
+  },
+  {
+    name: "HTML/CSS",
+    value: 85,
+  },
+  {
+    name: "React-Native",
+    value: 60,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+
+  {
+    name: "Node and Express",
+    value: 60,
+  },
+];
+
+const services = [
+  {
+    title: "Wordpress Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://www.netsolutions.com/insights/wp-content/uploads/2021/11/why-your-business-needs-a-mobile-eCommerce-app.jpg",
+    desctiption:
+      "Build a custom eCommerce platform with React, Redux, Node, Express and MongoDB",
+    link: "http://e-commerse-2000-mern.herokuapp.com/",
+  },
+  {
+    img: "https://i.pinimg.com/736x/ac/a9/a3/aca9a3f374aa8709bcf8b641cb6ff45e--logo-pinterest-follow-me.jpg",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "https://social-mediaclone.netlify.app/",
+  },
+  {
+    img: "https://github.com/Luci04/Sorting-Visualizer/raw/master/Sorting.gif",
+    desctiption:
+      "React Based Application Where You Can see what happen Over an Array when a Sorting Algo. is Applied",
+    link: "https://luci04.github.io/Sorting-Visualizer/",
+  },
+  // {
+  //   img: "https://picsum.photos/400/600/?grayscale",
+  //   desctiption:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+  // {
+  //   img: "https://picsum.photos/400/300/?grayscale",
+  //   desctiption:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+  // {
+  //   img: "https://picsum.photos/400/700/?grayscale",
+  //   desctiption:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+
+  // {
+  //   img: "https://picsum.photos/400/600/?grayscale",
+  //   desctiption:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+  // {
+  //   img: "https://picsum.photos/400/300/?grayscale",
+  //   desctiption:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+  // {
+  //   img: "https://picsum.photos/400/?grayscale",
+  //   desctiption:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+  // {
+  //   img: "https://picsum.photos/400/550/?grayscale",
+  //   desctiption:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+  // {
+  //   img: "https://picsum.photos/400/?grayscale",
+  //   desctiption:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+  // {
+  //   img: "https://picsum.photos/400/700/?grayscale",
+  //   desctiption:
+  //     "The wisdom of life consists in the elimination of non-essentials.",
+  //   link: "#",
+  // },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "avinashukla2000@gmail.com",
+  YOUR_FONE: "+91-8827040821",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_whqqvgn",
+  YOUR_TEMPLATE_ID: "template_g3lvh2q",
+  YOUR_USER_ID: "_N_YVow0XZ97Jlxps",
+};
+
+const socialprofils = {
+  github: "https://github.com/luci04",
+  linkedin: "https://linkedin.com/in/avinashukla",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
