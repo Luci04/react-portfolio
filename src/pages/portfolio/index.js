1c83ed7a94094634479c982029e5360e831855a0
Avinash Shukla <avinashukla2000@gmail.com>
2022-12-13 00:41:02 +0530

    Added tags Projects

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Container, Row, Col } from "react-bootstrap";
-import { dataportfolio, meta } from "../../content_option";
+import { dataportfolio, meta, languages } from "../../content_option";
 
 export const Portfolio = () => {
+
+  const [selected, setSelected] = useState("all");
+
+
+  const handleClick = (arg) => {
+    setSelected(arg);
+  }
+
   return (
     <HelmetProvider>
       <Container className="About-header">
@@ -13,26 +21,35 @@ export const Portfolio = () => {
           <title> Projects | {meta.title} </title>{" "}
           <meta name="description" content={meta.description} />
         </Helmet>
-        <Row className="mb-5 mt-3">
+        <Row className="mb-3 mt-3">
           <Col lg="8">
             <h1 className="display-4 mb-4"> Projects </h1>{" "}
             <hr className="t_border my-4 ml-0 text-left" />
           </Col>
+
+          <Col lg="8" onClick={(e) => console.log(e)} >
+            {
+              languages.map((ele) => {
+                return <button key={ele} onClick={() => handleClick(ele)} className={`po_btn ${selected === ele ? "po_btn_selected" : ""}`}>{ele}</button>
+              })
+            }
+          </Col>
+
         </Row>
         <div className="mb-5 po_items_ho">
-          {dataportfolio.map((data, i) => {
-            return (
-              <div key={i} className="po_item">
-                <img src={data.img} alt="" />
-                <div className="content">
-                  <p>{data.desctiption}</p>
-                  <a target="_blank" rel="noopener noreferrer" href={data.link}>
-                    view project
-                  </a>
-                </div>
+          {dataportfolio.filter((data) => data.tags.includes(selected)).map((data, i) => {
+            return (<div key={i} className="po_item">
+              <img src={data.img} alt="" />
+              <div className="content">
+                <p>{data.desctiption}</p>
+                <a target="_blank" rel="noopener noreferrer" href={data.link}>
+                  view project
+                </a>
               </div>
-            );
-          })}
+            </div>
+            )
+          })
+          }
         </div>
       </Container>
     </HelmetProvider>
